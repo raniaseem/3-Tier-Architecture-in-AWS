b35cdd63f937320b66129191f8af3993bec4585b
Rani Aseem <150495420+raniaseem@users.noreply.github.com>
2026-06-21 20:06:58 +0530

    Update Dbconfig.js

--- Dbconfig.js
+++ Dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-  DB_HOST : '',
-  DB_USER : '',
-  DB_PWD : '',
-  DB_DATABASE : '',
-}); 
+  DB_HOST : 'database-1-instance-1.cqhu46i60ot2.us-east-1.rds.amazonaws.com',
+  DB_USER : 'admin',
+  DB_PWD : 'YOUR_REAL_AURORA_PASSWORD',
+  DB_DATABASE : 'webappdb',
+});
